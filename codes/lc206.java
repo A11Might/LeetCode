6d727bcc0d69c2d16ffc1d308355f73f7053ae3d
Kohath Hu <qihanghu@foxmail.com>
2019-05-28 14:58:52 +0800

    Update lc206.java

--- codes/lc206.java
+++ codes/lc206.java
@@ -4,7 +4,7 @@
  * [206] 反转链表
  * 
  * 迭代：使用三个指针pre、cur和succ
- * 递归：f(head) = head和head.next交换 + f(head.next)
+ * 递归：f(head) = 反转head和head.next之间的链式结果 + f(head.next)
  */
 /**
  * Definition for singly-linked list.
