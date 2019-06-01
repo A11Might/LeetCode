b538c2dce0caae25da3a7317415af5f709f613fc
Kohath Hu <qihanghu@foxmail.com>
2019-06-01 18:59:55 +0800

    Create lc039.java

---
+++ codes/lc039.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=39 lang=java
+ *
+ * [39] 组合总和
+ * 
+ * 回溯
+ */
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(res, new ArrayList<>(), nums, target, 0);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, List<Integer> tempList, int[] nums, int target, int index) {
+        if (target < 0) {
+            return;
+        } else if (target == 0) {
+            res.add(new ArrayList<>(tempList)); // ???
+        } else {
+            // index相等于背包问题的放与不放但还有放多个(即index不变)
+            for (int i = index; i < nums.length; ++i) {
+                tempList.add(nums[i]);
+                backtrack(res, tempList, nums, target - nums[i], i); // index == i，数组中的元素可以被重复选取
+                tempList.remove(tempList.size() - 1); // 还原tempList，防止对下一步递归产生影响
+            }
+        }
+    }
+}
