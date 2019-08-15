517aeb34a41131acce0a3d81c0fc949aef3590a6
Qihang Hu <qihanghu@foxmail.com>
2019-08-15 10:12:58 +0800

    Update lc053.java

--- codes/lc053.java
+++ codes/lc053.java
@@ -3,23 +3,33 @@
  *
  * [53] 最大子序和
  * 
- * 对于含有正数的序列而言，最大子序列肯定是正数，所以头尾肯定都是正数
- * 从第一个正数开始算起，每往后加一个数便更新一次和的最大值
- * 当当前和成为负数时，则表明此前序列无法为后面提供最大子序列和，重新确定序列首项.
+ * 题目：给一个数组，返回它的最大连续子序列的和(要求时间复杂度O(n))
+ * 
+ * 思路：动态规划，f(i)表示以i个元素结尾的子数组的最大和
+ *       f(i) = array[i] + f(i - 1) if f(i - 1) > 0
  */
 class Solution {
     public int maxSubArray(int[] nums) {
-        int res = nums[0], sum = 0; // res为最大子序和，sum为当前子序和
-        for (int i = 0; i < nums.length; ++i) {
-            // 若sum小于0时，当前子序和对最大子序和无作用，重新计算子序和
-            if (sum < 0) {
-                sum = nums[i];
-            } else {
-                sum += nums[i];
-            }
-            // 时刻更新最大子序和
-            res = Math.max(res, sum);
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        // f(i) = array[i] + f(i - 1) if f(i - 1) > 0
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = nums[i];
+            if (dp[i - 1] > 0) {
+                dp[i] += dp[i - 1];
+            }
+        }
+
+        // find larget subarray sum
+        int res = Integer.MIN_VALUE;
+        for (int num : dp) {
+            System.out.println(num);
+            res = Math.max(res, num);
+        }
+        System.out.println(res);
         return res;
     }
 }
