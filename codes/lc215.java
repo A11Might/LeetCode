034d1c73836cf3d5d7326b74d270e0cc793a1a51
Qihang Hu <qihanghu@foxmail.com>
2019-09-04 19:28:49 +0800

    Update lc215.java

--- codes/lc215.java
+++ codes/lc215.java
@@ -11,10 +11,11 @@ import java.util.Queue;
  * 
  * 思路：
  * 1、大小为k的小根堆
- * 2、bfprt算法
+ * 2、利用快排partition分割数组，分割位置为n - k时，找到第k个最大的元素
+ * 3、bfprt算法
  */
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
+    public int findKthLargest1(int[] nums, int k) {
         // 处理k < 1 || k > nums.length的特殊情况
         if (k < 1 || k > nums.length) {
             int min = Integer.MAX_VALUE;
@@ -51,6 +52,48 @@ class Solution {
             return o1 - o2;
         }
 
+    }
+    
+    public int findKthLargest2(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || nums.length < k || k == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int target = n - k; // 目标元素的索引
+        // 在数组中寻找位置为target的pivot
+        int start = 0, end = n - 1;
+        int index = partition(nums, start, end);
+        while (index != target) {
+            if (index < target) {
+                start = index + 1;
+            } else {
+                end = index - 1;
+            }
+            index = partition(nums, start, end);
+        }
+        
+        return nums[index];
+    }
+    
+    // 返回pivot位置的索引
+    private int partition(int[] arr, int left, int right) {
+        int n = right - left + 1;
+        swap(arr, right, left + (int) Math.random() * n);
+        int pivot = arr[right];
+        int small = left - 1;
+        for (int i = left; i < right; i++) {
+            if (arr[i] < pivot) {
+                swap(arr, i, ++small);
+            }
+        }
+        swap(arr, right, ++small);
+        return small;
+    }
+    
+    private void swap(int[] arr, int a, int b) {
+        int temp = arr[b];
+        arr[b] = arr[a];
+        arr[a] = temp;
     }
 }
 
