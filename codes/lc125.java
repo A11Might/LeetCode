2a397c510cd22463570e372772f7e91949940f22
Qihang Hu <qihanghu@foxmail.com>
2019-09-04 20:01:29 +0800

    Update lc125.java

--- codes/lc125.java
+++ codes/lc125.java
@@ -3,11 +3,10 @@
  *
  * [125] 验证回文串
  * 
- * 1、指针碰撞，两个指针跳过不是小写字母和数字的字符
- * 2、使用while加速跳过(连续跳过多个非法字符) 快一倍
+ * 指针碰撞，两个指针跳过不是小写字母和数字的字符
  */
 class Solution {
-    public boolean isPalindrome2(String s) {
+    public boolean isPalindrome(String s) {
         if (s == null) {
             return true;
         }
@@ -35,28 +34,8 @@ class Solution {
         if (chrs[index] >= 'a' && chrs[index] <= 'z' || chrs[index] >= '0' && chrs[index] <= '9') {
             return true;
         }
+        
         return false;
-    }
-
-    public boolean isPalindrome(String s) {
-        if (s == null) {
-            return true;
-        }
-        s = s.toLowerCase();
-        char[] chrsS = s.toCharArray();
-        int l = 0, r = chrsS.length - 1;
-        while (l < r) {
-            if (!isValid(chrsS, l)) {
-                l++;
-            } else if (!isValid(chrsS, r)) {
-                r--;
-            } else if (isValid(chrsS, l) && isValid(chrsS, r)) {
-                if (chrsS[l++] != chrsS[r--]) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
 
