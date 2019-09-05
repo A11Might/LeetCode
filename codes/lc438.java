496fd3bcd950ec02c688399cfd02098c4a525143
Qihang Hu <qihanghu@foxmail.com>
2019-09-05 10:49:01 +0800

    Update lc438.java

--- codes/lc438.java
+++ codes/lc438.java
@@ -6,64 +6,74 @@ import java.util.List;
  *
  * [438] 找到字符串中所有字母异位词
  * 
- * 1、暴力：设置p.length()的窗口在s上滑动，判断窗口中的字符串与p是否为字母异位词,不能跳过得一个一个滑动(很难跳过？)
- * 2、窗口和双指针(可以跳过，一次滑动很多)
+ * 题目：找到给定字符串s中所有非空字符串p的字母异位词的子串，返回这些子串的起始索引
+ * 
+ * 思路：1、固定滑动窗口，设置p.length()的窗口在s上滑动，判断窗口中的字符串与p是否为字母异位词,不能跳过得一个一个滑动(很难跳过？)
+ *       2、滑动窗口，维持窗口中的字母不超过p，字母种类，单一字母个数(可以跳过，一次滑动很多)
  */
 class Solution {
     public List<Integer> findAnagrams1(String s, String p) {
-        int lenP = p.length();
-        if (s == null || lenP > s.length()) {
-            return new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        if (s.length() < p.length()) {
+            return res;
         }
         int[] times = new int[26];
         for (char chr : p.toCharArray()) {
             times[chr - 'a']++;
         }
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i <= s.length() - lenP; ++i) {
-            if (findOneAnagram(s,times, i, lenP)) {
-                res.add(i);
+        int[] curTimes = new int[26];
+        int l = 0, r = -1;
+        while (r + 1 < s.length()) {
+            curTimes[s.charAt(++r) - 'a']++;
+            // 窗口固定为p的长度
+            if (r - l + 1 > p.length()) {
+                curTimes[s.charAt(l++) - 'a']--;
+            }
+            // 判断每个窗口中的字符串与p是否为字母异位词
+            if (r - l + 1 == p.length() && same(times, curTimes)) {
+                res.add(l);
             }
         }
+        
         return res;
     }
-
-    private boolean findOneAnagram(String s, int[] touchedP, int index, int len) {
-        int[] touchedS = new int[26];
-        for (int i = index; i < index + len; ++i) {
-            touchedS[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < 26; ++i) {
-            if (touchedS[i] != touchedP[i]) {
+    
+    private boolean same(int[] a, int[] b) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
                 return false;
             }
         }
+        
         return true;
     }
 
     public List<Integer> findAnagrams2(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-        int[] timesP = new int[26];
+        ArrayList<Integer> res = new ArrayList<>();
+        if (s.length() < p.length()) {
+            return res;
+        }
+        int[] times = new int[26];
         for (char chr : p.toCharArray()) {
-            timesP[chr - 'a']++;
+            times[chr - 'a']++;
         }
-        int start = 0, end = 0; // 窗口中的两个指针
-        int[] timesBetween = new int[26]; // 记录两指针之间的字符出现次数
-        while (end < s.length()) {
-            // 注册s串中当前字符
-            int curChrIndex = s.charAt(end++) - 'a'; 
-            timesBetween[curChrIndex]++;
-            // 窗口中s串的当前字符数量大于p串，说明该窗口组成的字符串与p不是字母异位词，重置窗口
-            while (timesBetween[curChrIndex] > timesP[curChrIndex]) {
-                timesBetween[s.charAt(start++) - 'a']--; // 当前元素若是非p中元素，则最后start在当前不同元素之后；当前元素若是p中元素，则最后start在当前窗口中第一个该元素之上
+        int[] curTimes = new int[26];
+        int l = 0, r = -1;
+        while (r + 1 < s.length()) {
+            char cur = s.charAt(++r);
+            curTimes[cur - 'a']++;
+            // 维持窗口中的字母不超过p
+            // 当窗口中的当前字母个数超过p时，通过移动左边界来去除该多余字母
+            while (curTimes[cur - 'a'] > times[cur - 'a']) {
+                curTimes[s.charAt(l++) - 'a']--;
             }
-            // 注册s串中当前字符时end++，所以end - start为p.length时窗口组成的字符串与p是字母异位词
-            if (end - start == p.length()) {
-                res.add(start);
+            // 形成窗口，记录字母异位词起始索引
+            if (r - l + 1 == p.length()) {
+                res.add(l);
             }
         }
+        
         return res;
     }
-
 }
 
