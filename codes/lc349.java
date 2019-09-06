bead8f288f052ea7348cfd898f0d95e3495704ef
Qihang Hu <qihanghu@foxmail.com>
2019-09-06 15:32:52 +0800

    Create lc349.java

---
+++ codes/lc349.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+
+/*
+ * @lc app=leetcode.cn id=349 lang=java
+ *
+ * [349] 两个数组的交集
+ * 
+ * 题目：计算给定两个数组的交集(输出结果中的每个元素一定是唯一的，不考虑输出结果的顺序)
+ * 
+ * 思路：查找表set
+ */
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> record = new HashSet<>();
+        HashSet<Integer> resultSet = new HashSet<>();
+        // 注册nums1中的所有数字
+        for (int num : nums1) {
+            record.add(num);
+        }
+
+        // 遍历nums2找到与num1的交集，放入set中去重
+        for (int num : nums2) {
+            if (record.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int index = 0;
+        int[] res = new int[resultSet.size()];
+        for (int num : resultSet) {
+            res[index++] = num;
+        }
+
+        return res; 
+    }
+}
+
