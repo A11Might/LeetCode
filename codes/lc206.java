4ace158a7cca8204f60e916658dae672c2dd31e4
Qihang Hu <qihanghu@foxmail.com>
2019-09-12 15:24:32 +0800

    Update lc206.java

--- codes/lc206.java
+++ codes/lc206.java
@@ -3,8 +3,10 @@
  *
  * [206] 反转链表
  * 
- * 迭代：使用三个指针pre、cur和succ
- * 递归：f(head) = 反转head和head.next之间的链式结构 + f(head.next)
+ * 题目：反转一个单链表
+ * 
+ * 思路：1、迭代：使用三个指针pre、cur和succ
+ *      2、递归：f(head) = 反转head和head.next之间的链式结构 + f(head.next)
  */
 /**
  * Definition for singly-linked list.
@@ -15,21 +17,20 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList1(ListNode head) {
         if (head == null) {
             return null;
         }
-        if (head.next == null) {
-            return head;
-        }
         ListNode pre = null;
         ListNode cur = head;
+        ListNode succ = null;
         while (cur != null) {
-            ListNode succ = cur.next;
+            succ = cur.next;
             cur.next = pre;
             pre = cur;
             cur = succ;
         }
+
         return pre;
     }
 
@@ -37,19 +38,17 @@ class Solution {
         if (head == null) {
             return null;
         }
-        if (head.next == null) {
-            return head;
-        }
         return process(head);
     }
 
-    private ListNode process(ListNode head) {
-        if (head.next == null) {
-            return head;
+    private ListNode process(ListNode node) {
+        if (node.next == null) {
+            return node;
         }
-        ListNode res = process(head.next);
-        head.next.next = head;
-        head.next = null; // 让反转后最后一个节点的next为null
+        ListNode res = process(node.next);
+        node.next.next = node;
+        node.next = null;
+
         return res;
     }
 }
