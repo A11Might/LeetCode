12522a4a3b3464737a5158dc29cf7113abe2c10b
Qihang Hu <qihanghu@foxmail.com>
2019-09-15 23:26:41 +0800

    Create lc328.java

---
+++ codes/lc328.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=3 lang=java
+ *
+ * [328] 奇偶链表
+ * 
+ * 题目：将给定链表的所有奇数节点和偶数节点分别排在一起
+ *      (奇偶指的是节点编号，保持奇数节点和偶数节点的相对顺序)
+ * 
+ * 思路：类似快排的partition，使用虚拟节点将链表分为奇数节点和偶数节点两个链表
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        // 奇数节点部分的虚拟头节点
+        ListNode oddDummy = new ListNode(0);
+        ListNode oddTail = oddDummy;
+        // 偶数节点部分的虚拟头节点
+        ListNode evenDummy = new ListNode(0);
+        ListNode evenTail = evenDummy;
+        // 通过flag标识，依次先后将遍历的节点放入奇偶两个链表
+        boolean flag = true;
+        while (head != null) {
+            if (flag) {
+                oddTail.next = head;
+                oddTail = head;
+            } else {
+                evenTail.next = head;
+                evenTail = head;
+            }
+            head = head.next;
+            flag = !flag;
+        }
+        // 将两链表和并
+        oddTail.next = evenDummy.next;
+        // 需要将最后一个节点的next置为null，防止形成环而报错
+        // 最后一个节点可能连在奇数节点上，不置为null可能会形成环
+        evenTail.next = null;
+        
+        return oddDummy.next;
+    }
+}
+
