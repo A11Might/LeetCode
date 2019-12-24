682148966f370894532f8ace3399b033aa1fe10f
qihang hu <qihanghu@foxmail.com>
2019-12-24 09:59:35 +0800

    update 329

--- src/lc329.java
+++ src/lc329.java
@@ -3,59 +3,56 @@
  *
  * [329] 矩阵中的最长递增路径
  * 
- * 题意：给定一个整数矩阵，找出最长递增路径的长度(可以往上，下，左，右四个方向移动)
+ * 题目: 给定一个整数矩阵，找出最长递增路径的长度(可以往上，下，左，右四个方向移动)
+ *
+ * 难度: hard
  * 
  * https://www.bilibili.com/video/av48832928
- * 思路：动态规划，f(a, b) = Math.max(f(a, b - 1), f(a, b + 1), f(a - 1, b), f(a + 1, b))
+ * 思路：dfs, 利用之前搜索的结果, 简化这次搜索操作
  */
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
-            return 0;
-        }
+        if (matrix == null || matrix.length == 0) return 0;
         int rows = matrix.length, cols = matrix[0].length;
+        // dp[i][j]为从[i, j]出发能找到的最长递增路径的长度
         int[][] dp = new int[rows][cols];
-
-        int res = 0;
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (dp[i][j] == 0) {
-                    dfs(matrix, i, j, dp, Integer.MIN_VALUE);
-                    res = Math.max(res, dp[i][j]);
-                }
+        int res = 0; // 最长递增路径长度
+        // 从矩阵中的每个点开始dfs, 并更新当前最长递增路径长度
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                dfs(matrix, i, j, dp, Integer.MIN_VALUE);
+                res = Math.max(res, dp[i][j]);
             }
         }
 
         return res;
     }
 
-    /**
-     * 从当前元素向上下左右四个方向，dfs
-     * 
-     * @param matrix
-     * @param row 当前元素所在行
-     * @param col 当前元素所在列
-     * @param dp 
-     * @param prev 前一个元素的大小，用于比较是否满足递增条件
-     * @return
-     */
-    private int dfs(int[][] matrix, int row, int col, int[][] dp, int prev) {
-        if (row < 0 || row > matrix.length - 1 ||
-            col < 0 || col > matrix[0].length - 1 ||
-            matrix[row][col] <= prev) {
-                return 0;
-        }
-        if (dp[row][col] != 0) {
-            return dp[row][col];
-        }
+    // dfs, 从[row][col]出发找的最长递增路径的长度
+    // pre记录上个节点的值, 用于判断是否男足递增路径的条件(初始化为Integer.MIN_VALUE)
+    private int dfs(int[][] matrix, int row, int col, int[][] dp, int pre) {
+        // 越界或不满足递增条件
+        if (row >= matrix.length || row < 0 ||
+                col >= matrix[0].length || col < 0 ||
+                matrix[row][col] <= pre) return 0;
+        // 当前位置dfs过, 最长递增路径的长度已知, 直接返回
+        if (dp[row][col] != 0) return dp[row][col];
 
-        // f(a, b) = Math.max(f(a, b - 1), f(a, b + 1), f(a - 1, b), f(a + 1, b))
-        int left = dfs(matrix, row, col - 1, dp, matrix[row][col]);
-        int right = dfs(matrix, row, col + 1, dp, matrix[row][col]);
-        int up = dfs(matrix, row - 1, col, dp, matrix[row][col]);
-        int down = dfs(matrix, row + 1, col, dp, matrix[row][col]);
+        /**
+         * [
+         *   [(9), 9, 4],
+         *   [(6), 6, 8],
+         *   [(2),(1),1]
+         * ]
+         * 则f(2, 1) = f(2, 0) + 1
+         */
+        int left = dfs(matrix, row - 1, col, dp, matrix[row][col]);
+        int right = dfs(matrix, row + 1, col, dp, matrix[row][col]);
+        int up = dfs(matrix, row, col - 1, dp, matrix[row][col]);
+        int down = dfs(matrix, row, col + 1, dp, matrix[row][col]);
+        // f(a, b) = Math.max(f(a, b - 1), f(a, b + 1), f(a - 1, b), f(a + 1, b)) + 1
+        dp[row][col] = 1 + Math.max(left, Math.max(right, Math.max(up, down)));
 
-        dp[row][col] = Math.max(left, Math.max(right, Math.max(up, down))) + 1;
         return dp[row][col];
     }
 }
